9090d82d5e010854624ce010753c08d5df88770e
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-22 18:17:12 -0300

    Makes course class 'Mais operações de conjuntos'

---
+++ src/aulas/02.py
@@ -0,0 +1,12 @@
+usurarios_data_science = {15, 23, 43, 56}
+usurarios_machine_learning = {13, 23, 43, 56}
+
+print(usurarios_data_science | usurarios_machine_learning)
+
+print(usurarios_data_science & usurarios_machine_learning)
+
+# Subtração de conjunto
+print(usurarios_data_science - usurarios_machine_learning)
+
+# OU exclusivo
+print(usurarios_data_science ^ usurarios_machine_learning)
