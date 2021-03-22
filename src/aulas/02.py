70e4d10f65917132c56c3965b701c201872bb953
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-22 18:19:31 -0300

    Makes course class 'Mais operações de conjuntos'

--- src/aulas/02.py
+++ src/aulas/02.py
@@ -1,12 +1,14 @@
 usurarios_data_science = {15, 23, 43, 56}
 usurarios_machine_learning = {13, 23, 43, 56}
 
+# Utilizar o | para juntar conjuntos
 print(usurarios_data_science | usurarios_machine_learning)
 
+# Utilizar o & para juntar apenas números que estão no mesmo conjunto
 print(usurarios_data_science & usurarios_machine_learning)
 
-# Subtração de conjunto
+# Utilizar o - para remover números repetidos que estão no em dois conjuntos
 print(usurarios_data_science - usurarios_machine_learning)
 
-# OU exclusivo
+# ou (^) exclusivo
 print(usurarios_data_science ^ usurarios_machine_learning)
